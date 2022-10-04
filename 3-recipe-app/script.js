9af99d0784dbe32d45abd58b6196c4cf57ec55cf
arif <arif-celik56@hotmail.com>
2022-10-05 02:51:15 +0300

    i forgot this

--- 3-recipe-app/script.js
+++ 3-recipe-app/script.js
@@ -1,13 +1,5 @@
 import Controller from './Controller.js';
 import Fetcher from './Fetcher.js';
-import Meal from './Meal.js';
-
-const hamb = document.querySelector('.hamb');
-const nav = document.querySelector('nav');
-const logo = document.querySelector('.logo');
-const searchSVG = document.querySelector('.search-div .icon');
-const searchBar = document.querySelector('input#searchbar');
-const categoryList = document.querySelector('nav ul');
 
 const controller = new Controller();
 const fetcher = new Fetcher();
@@ -15,16 +7,9 @@ const fetcher = new Fetcher();
 let categories = await fetcher.fetchCategories();
 controller.setCategories(categories);
 
-/**
- * its takes category name from innerHTML 
- * @param {String} name 
- */
 window.getMealsByCategory = async function (name) {
     let data = await fetcher.fetchMealsByCategory(name)
     console.log(data)
 
     controller.listMeals(data)
 }
-/* 
-let details = await fetcher.fetchMealDetailsById(52895)
-console.log(details['meals'][0]) */
